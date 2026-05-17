1ecef41951604ab47d7c521e357e664c4c82dacf
roman23323 <ignatenko_roman.s@mail.ru>
2026-05-17 21:13:42 +0300

    feat: add quiz create, find mine and public quizzes logic

--- src/quizzes/quizzes.service.ts
+++ src/quizzes/quizzes.service.ts
@@ -1,4 +1,49 @@
 import { Injectable } from '@nestjs/common';
 
+import { PrismaService } from '../prisma/prisma.service';
+
+import { CreateQuizDto } from './dto/create-quiz.dto';
+
 @Injectable()
-export class QuizzesService {}
+export class QuizzesService {
+  constructor(private prisma: PrismaService) {}
+
+  async create(authorId: string, dto: CreateQuizDto) {
+    return this.prisma.quiz.create({
+      data: {
+        authorId,
+
+        title: dto.title,
+        description: dto.description,
+
+        visibility: dto.visibility,
+
+        secondsPerQuestion: dto.secondsPerQuestion,
+      },
+    });
+  }
+
+  async findMyQuizzes(userId: string) {
+    return this.prisma.quiz.findMany({
+      where: {
+        authorId: userId,
+      },
+
+      orderBy: {
+        createdAt: 'desc',
+      },
+    });
+  }
+
+  async findPublicQuizzes() {
+    return this.prisma.quiz.findMany({
+      where: {
+        visibility: 'public',
+      },
+
+      orderBy: {
+        createdAt: 'desc',
+      },
+    });
+  }
+}
