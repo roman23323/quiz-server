7c96e4203b64d43cf762e00936d560c50287c378
roman23323 <ignatenko_roman.s@mail.ru>
2026-05-17 22:25:36 +0300

    feat(quizzes): add CRU routes Delete not included

--- src/quizzes/quizzes.controller.ts
+++ src/quizzes/quizzes.controller.ts
@@ -1,9 +1,11 @@
 import {
-  Body,
-  Controller,
-  Get,
-  Post,
-  UseGuards,
+    Body,
+    Controller,
+    Get,
+    Param,
+    Patch,
+    Post,
+    UseGuards,
 } from '@nestjs/common';
 
 import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
@@ -13,28 +15,79 @@ import { CurrentUser } from '../auth/decorators/current-user.decorator';
 import { QuizzesService } from './quizzes.service';
 
 import { CreateQuizDto } from './dto/create-quiz.dto';
+import { UpdateQuizDto } from './dto/update-quiz.dto';
+import { CreateQuestionDto } from './dto/create-question.dto';
 
 @Controller('quizzes')
 export class QuizzesController {
-  constructor(private quizzesService: QuizzesService) {}
+    constructor(private quizzesService: QuizzesService) { }
 
-  @UseGuards(JwtAuthGuard)
-  @Post()
-  createQuiz(
-    @CurrentUser() user: any,
-    @Body() dto: CreateQuizDto,
-  ) {
-    return this.quizzesService.create(user.id, dto);
-  }
+    @UseGuards(JwtAuthGuard)
+    @Post()
+    createQuiz(
+        @CurrentUser() user: any,
+        @Body() dto: CreateQuizDto,
+    ) {
+        return this.quizzesService.create(user.id, dto);
+    }
 
-  @UseGuards(JwtAuthGuard)
-  @Get('my')
-  getMyQuizzes(@CurrentUser() user: any) {
-    return this.quizzesService.findMyQuizzes(user.id);
-  }
+    @UseGuards(JwtAuthGuard)
+    @Get('my')
+    getMyQuizzes(@CurrentUser() user: any) {
+        return this.quizzesService.findMyQuizzes(user.id);
+    }
 
-  @Get('public')
-  getPublicQuizzes() {
-    return this.quizzesService.findPublicQuizzes();
-  }
+    @Get('public')
+    getPublicQuizzes() {
+        return this.quizzesService.findPublicQuizzes();
+    }
+
+    @UseGuards(JwtAuthGuard)
+    @Patch(':id')
+    updateQuiz(
+        @Param('id') quizId: string,
+        @CurrentUser() user: any,
+        @Body() dto: UpdateQuizDto
+    ) {
+        return this.quizzesService.update(
+            quizId,
+            user.id,
+            dto
+        )
+    }
+
+    @UseGuards(JwtAuthGuard)
+    @Post(':id/questions')
+    addQuestion(
+        @Param('id') quizId: string,
+        @CurrentUser() user: any,
+        @Body() dto: CreateQuestionDto,
+    ) {
+        return this.quizzesService.addQuestion(
+            quizId,
+            user.id,
+            dto,
+        );
+    }
+
+    @UseGuards(JwtAuthGuard)
+    @Get(':id/edit')
+    getQuizForEdit(
+        @Param('id') quizId: string,
+        @CurrentUser() user: any,
+    ) {
+        return this.quizzesService.findOneForEdit(
+            quizId,
+            user.id,
+        );
+    }
+
+    @Get(':id/play')
+    getQuizForPlay(
+        @Param('id') quizId: string,
+    ) {
+        return this.quizzesService.findOneForPlay(
+            quizId,
+        );
+    }
 }
