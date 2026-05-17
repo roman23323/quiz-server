c85a6ddfcb1631816302d75ced892e5ed7ae5507
roman23323 <ignatenko_roman.s@mail.ru>
2026-05-18 02:10:36 +0300

    feat(sessions): add create session DTO

---
+++ src/sessions/dto/create-session.dto.ts
@@ -0,0 +1,10 @@
+import { IsEnum, IsUUID } from 'class-validator';
+import { SessionMode } from '@prisma/client';
+
+export class CreateSessionDto {
+    @IsUUID()
+    quizId: string;
+
+    @IsEnum(SessionMode)
+    mode: SessionMode;
+}
