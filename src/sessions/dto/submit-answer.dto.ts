a96413874de6044cba17b62a91998bf6080feede
roman23323 <ignatenko_roman.s@mail.ru>
2026-05-18 02:11:06 +0300

    feat(sessions): add submit answer DTO

---
+++ src/sessions/dto/submit-answer.dto.ts
@@ -0,0 +1,24 @@
+import {
+    IsInt,
+    IsOptional,
+    IsString,
+    IsUUID,
+    Min,
+} from 'class-validator';
+
+export class SubmitAnswerDto {
+    @IsUUID()
+    questionId: string;
+
+    @IsOptional()
+    @IsUUID()
+    selectedOptionId?: string;
+
+    @IsOptional()
+    @IsString()
+    textAnswer?: string;
+
+    @IsInt()
+    @Min(0)
+    responseTimeMs: number;
+}
