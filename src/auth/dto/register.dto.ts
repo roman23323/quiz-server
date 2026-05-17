c16b7c383b58992b03b35b1f9fb7065c42fe576f
roman23323 <ignatenko_roman.s@mail.ru>
2026-05-17 20:39:27 +0300

    feat: add login and register DTOs

---
+++ src/auth/dto/register.dto.ts
@@ -0,0 +1,13 @@
+import { IsString, MaxLength, MinLength } from 'class-validator';
+
+export class RegisterDto {
+    @IsString()
+    @MinLength(3)
+    @MaxLength(32)
+    name: string;
+
+    @IsString()
+    @MinLength(6)
+    @MaxLength(128)
+    password: string;
+}
