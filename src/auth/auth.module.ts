41de5b5cd5a4f61d143b87af1e2fce8390b1be1e
roman23323 <ignatenko_roman.s@mail.ru>
2026-05-17 20:53:46 +0300

    feat: import dependency modules

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,9 +2,24 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { UsersModule } from 'src/users/users.module';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    UsersModule,
+    PassportModule,
+    JwtModule.registerAsync({
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET'),
+        signOptions: {
+          expiresIn: configService.get('JWT_EXPIRES_IN')
+        }
+      })
+    })
+  ],
   providers: [AuthService],
   controllers: [AuthController]
 })
