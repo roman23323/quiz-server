eb9ec958a13b482ed222fff3da25c6d3597c98d9
roman23323 <ignatenko_roman.s@mail.ru>
2026-05-18 02:14:13 +0300

    feat(sessions): add sessions routes

--- src/sessions/sessions.controller.ts
+++ src/sessions/sessions.controller.ts
@@ -1,4 +1,72 @@
-import { Controller } from '@nestjs/common';
+import {
+    Body,
+    Controller,
+    Get,
+    Param,
+    Post,
+    UseGuards,
+} from '@nestjs/common';
+
+import { SessionsService } from './sessions.service';
+import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
+import { CurrentUser } from '../auth/decorators/current-user.decorator';
+
+import { CreateSessionDto } from './dto/create-session.dto';
+import { SubmitAnswerDto } from './dto/submit-answer.dto';
 
 @Controller('sessions')
-export class SessionsController {}
+export class SessionsController {
+    constructor(
+        private readonly sessionsService: SessionsService,
+    ) { }
+
+    @UseGuards(JwtAuthGuard)
+    @Post()
+    createSession(
+        @CurrentUser() user,
+        @Body() dto: CreateSessionDto,
+    ) {
+        return this.sessionsService.createSession(
+            user.id,
+            dto,
+        );
+    }
+
+    @UseGuards(JwtAuthGuard)
+    @Get(':id/current-question')
+    getCurrentQuestion(
+        @CurrentUser() user,
+        @Param('id') sessionId: string,
+    ) {
+        return this.sessionsService.getCurrentQuestion(
+            user.id,
+            sessionId,
+        );
+    }
+
+    @UseGuards(JwtAuthGuard)
+    @Post(':id/answer')
+    submitAnswer(
+        @CurrentUser() user,
+        @Param('id') sessionId: string,
+        @Body() dto: SubmitAnswerDto,
+    ) {
+        return this.sessionsService.submitAnswer(
+            user.id,
+            sessionId,
+            dto,
+        );
+    }
+
+    @UseGuards(JwtAuthGuard)
+    @Get(':id/result')
+    getResult(
+        @CurrentUser() user,
+        @Param('id') sessionId: string,
+    ) {
+        return this.sessionsService.getResult(
+            user.id,
+            sessionId,
+        );
+    }
+}
