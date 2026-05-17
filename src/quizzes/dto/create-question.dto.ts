36f9a81d0b47ea3383f84232852d661f78d98535
roman23323 <ignatenko_roman.s@mail.ru>
2026-05-17 22:08:00 +0300

    feat(quizzes): add create question DTO

---
+++ src/quizzes/dto/create-question.dto.ts
@@ -0,0 +1,46 @@
+import {
+  ArrayMinSize,
+  IsArray,
+  IsEnum,
+  IsInt,
+  IsOptional,
+  IsString,
+  Min,
+  ValidateNested,
+} from 'class-validator';
+
+import { Type } from 'class-transformer';
+
+import { CreateQuestionOptionDto } from './create-question-option.dto';
+
+enum QuestionType {
+  single_choice = 'single_choice',
+  text = 'text',
+}
+
+export class CreateQuestionDto {
+  @IsString()
+  text: string;
+
+  @IsEnum(QuestionType)
+  questionType: QuestionType;
+
+  @IsOptional()
+  @IsString()
+  correctAnswer?: string;
+
+  @IsInt()
+  @Min(1)
+  points: number;
+
+  @IsInt()
+  @Min(0)
+  orderIndex: number;
+
+  @IsOptional()
+  @IsArray()
+  @ArrayMinSize(1)
+  @ValidateNested({ each: true })
+  @Type(() => CreateQuestionOptionDto)
+  options?: CreateQuestionOptionDto[];
+}
