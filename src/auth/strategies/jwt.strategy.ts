7364d249ab888dd6bf97680bee3de687d18844f9
roman23323 <ignatenko_roman.s@mail.ru>
2026-05-17 20:47:38 +0300

    feat: add JWT auth strategy

---
+++ src/auth/strategies/jwt.strategy.ts
@@ -0,0 +1,37 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+
+import { PrismaService } from '../../prisma/prisma.service';
+import { ConfigService } from '@nestjs/config';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+    constructor(
+        private readonly configService: ConfigService,
+        private readonly prisma: PrismaService
+    ) {
+    super({
+        jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+
+        ignoreExpiration: false,
+
+        secretOrKey: configService.get<string>('JWT_SECRET')!,
+    });
+  }
+
+    async validate(payload: { sub: string }) {
+        const user = await this.prisma.user.findUnique({
+            where: {
+                id: payload.sub,
+            },
+        });
+
+        if (!user) {
+            throw new UnauthorizedException();
+        }
+
+        return user;
+  }
+}
