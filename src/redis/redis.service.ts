80ebe2de21e01ee0470d1d333e2c2195bd40aa2d
roman23323 <ignatenko_roman.s@mail.ru>
2026-05-18 12:37:46 +0300

    feat(live): init redis client

--- src/redis/redis.service.ts
+++ src/redis/redis.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import Redis from 'ioredis';
 
 @Injectable()
-export class RedisService {}
+export class RedisService {
+    private readonly redis: Redis;
+
+    constructor(private readonly configModule: ConfigService) {
+        this.redis = new Redis({
+            host: configModule.get<string>('REDIS_URL'),
+            port: configModule.get<number>('REDIS_PORT')
+        });
+    }
+}
